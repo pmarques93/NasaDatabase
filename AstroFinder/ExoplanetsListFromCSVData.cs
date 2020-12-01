27cdba1cecd6087f06470d9faab36ea0a99e750f
JundMaster <gabrielsantostroll@gmail.com>
2020-12-01 00:55:35 +0000

    Add independency of column order on file
    The program is no longer dependent of the orders the column appear.

--- AstroFinder/ExoplanetsListFromCSVData.cs
+++ AstroFinder/ExoplanetsListFromCSVData.cs
@@ -13,24 +13,39 @@ namespace AstroFinder
 
         public new ICollection GetCollection(string[] data)
         {
+            ICollection rawData = base.GetCollection(data);
 
-            IEnumerable<string[]> refinedData = base.GetCollection(data)
-                                                    .OfType<string[]>();
+            IEnumerable<string[]> queryableData = rawData.OfType<string[]>();
 
             Dictionary<string, int> headersDic = new Dictionary<string, int>();
 
-            for (int i = 0; i < Headers.Length; i++)
+            string[] orderedHeaders = new string[queryableData.ElementAt(0).Count()];
+
+            for (int i = 0; i < queryableData.ElementAt(0).Count(); i++)
             {
-                string planet = refinedData.ElementAt(0)[i];
-                if (planet.Contains(Headers[i]))
+                string header = queryableData.ElementAt(0)[i];
+
+                for (int j = 0; j < Headers.Length; j++)
                 {
-                    headersDic.Add(Headers[i], i);
+                    if (Headers[j] == header.Trim())
+                    {
+                       System.Console.WriteLine($"Header[{j}] - {Headers[j]} is on Column {i}");
+                       headersDic.Add(Headers[j],i);
+                    }
+                    // System.Console.WriteLine("\n");
                 }
             }
+
+            foreach (var v in headersDic)
+            {
+                System.Console.WriteLine($"Key: {v.Key}, Value: {v.Value}");
+            }
+
             return
-                refinedData.
-                Select(p => new Exoplanet(p[headersDic["pl_name"]].Trim(' '),
-                                            p[headersDic["hostname"]].Trim(' '))).
+                queryableData.
+                Skip(1).
+                Select(p => new Exoplanet(p?[headersDic["pl_name"]].Trim(' '),
+                                            p?[headersDic["hostname"]].Trim(' '))).
                                             ToList();
         }
     }
