8b59754e7a03fc0a091a4ef3886c044aaebf06d0
JundMaster <gabrielsantostroll@gmail.com>
2020-12-03 21:03:32 +0000

    Implement the usage of table generation

--- AstroFinder/Data/StarsListFromCSVData.cs
+++ AstroFinder/Data/StarsListFromCSVData.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using AstroFinder.Data;
 
 namespace AstroFinder
 {
@@ -8,59 +10,26 @@ namespace AstroFinder
     {
         public StarsListFromCSVData(string[] headers) : base(headers) { }
 
-        public new ICollection GetCollection(string[] data)
-        {
-            // Retrieves the raw data from the CSV file
-            ICollection rawData = base.GetCollection(data);
+        public override ICollection GetCollection(string[] data)
+        {            
+            IGetDataTable<string[]> dataTableGetter 
+                        = new GetDataTableFromCSVData(data, null,
+                                                      HeadersOfInteress);
 
-            // Converts the raw data into queryable Data
-            // that makes it possible to make LINQ queries operations with it
-            IEnumerable<string[]> queryableData = rawData.OfType<string[]>();
+            DataTable dataTable = dataTableGetter.GetTableFromData();
 
-            // Dictionary that contain Column headers linked to their index
-            // in the file
-            // For instance: <pl_name, 0>
-            // This implies that the column header named 'pl_name'
-            // is the column of index 0 in the file
-            Dictionary<string, int> headersDic = new Dictionary<string, int>();
-
-            // Loops through the queryableData by columns
-            for (int i = 0; i < queryableData.ElementAt(0).Count(); i++)
-            {
-                // Gets the element of index 'i' of the line 0
-                // This means getting the column header of index 'i'
-                string header = queryableData.ElementAt(0)[i];
-
-                // Loops through all the headers of interess
-                for (int j = 0; j < HeadersOfInteress.Length; j++)
-                {
-                    // Checks if a certain header of interess of index 'j'
-                    // matches the header on the current column of the file
-                    if (HeadersOfInteress[j] == header.Trim())
-                    {
-                        // If the column header matches a header of interess
-                        // the header is added to the dictionary as a KEY
-                        // its value will be the header column index on the file
-                        headersDic.Add(HeadersOfInteress[j], i);
-                    }
-                }
-            }
-
-            // Returns a List of Exoplanets, skipping the first line
-            // of the data file that corresponds to the column headers
             return
-                queryableData.
-                Skip(1).
+                dataTable.AsEnumerable().
                 Select(p => new Star(
-                                p?[headersDic[HeadersOfInteress[0]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[1]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[2]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[3]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[4]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[5]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[6]]].Trim(),
-                                p?[headersDic[HeadersOfInteress[7]]].Trim())).
-                                ToList();
+                            p[0].ToString() != "" ? p[0].ToString() : null,
+                            p[1].ToString() != "" ? p[1].ToString() : null,
+                            p[2].ToString() != "" ? p[2].ToString() : null,
+                            p[3].ToString() != "" ? p[3].ToString() : null,
+                            p[4].ToString() != "" ? p[4].ToString() : null,
+                            p[5].ToString() != "" ? p[5].ToString() : null,
+                            p[6].ToString() != "" ? p[6].ToString() : null,
+                            p[7].ToString() != "" ? p[7].ToString() : null)).
+                            ToList();
         }
     }
 }
